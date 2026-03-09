db76aa5cae0ae8390621f557bd81d069f84f64e7
ngushteraklieva <ngushteraklieva@gmail.com>
2026-03-09 11:35:13 +0100

    Create Login Form

--- src/app/features/account/login/login.component.ts
+++ src/app/features/account/login/login.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { AccountService } from '../../../core/services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatCard,
+    MatButton,
+    MatInput,
+    MatFormField,
+    MatLabel
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private fb = inject(FormBuilder)
+  private accountService = inject(AccountService)
+  private router = inject(Router)
 
+  loginForm = this.fb.group({
+    email: [''],
+    password: ['']
+  })
+  
+  onSubmit(){
+    this.accountService.login(this.loginForm.value).subscribe({
+      next: () => {
+        this.accountService.getUserInfo();
+        this.router.navigateByUrl('/shop')
+      }
+    })
+  }
 }
